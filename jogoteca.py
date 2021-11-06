18388832559cea3be88918a73bfc5f5c6f1a1a93
gloriiakang <lori.vihchoi@gmail.com>
2021-11-06 13:37:36 -0300

    HTML para adicionar um novo jogo e nova rota no serv

--- jogoteca.py
+++ jogoteca.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
@@ -16,6 +17,11 @@ def ola():
     jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
     lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
 
 
 app.run()
