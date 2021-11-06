72528e2a14b9f5be181675a3441949ccafbe53fd
gloriiakang <lori.vihchoi@gmail.com>
2021-11-06 19:56:46 -0300

    Add deletar()

--- jogoteca.py
+++ jogoteca.py
@@ -57,6 +57,12 @@ def atualizar():
     return redirect(url_for('index'))
 
 
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    jogo_dao.deletar(id)
+    flash('O jogo foi removido com sucesso!')
+    return redirect(url_for('index'))
+
 @app.route('/login')
 def login():
     proxima = request.args.get('proxima')
