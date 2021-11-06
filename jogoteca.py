2bde9027d083932e9eb47360f6e61e232a495276
gloriiakang <lori.vihchoi@gmail.com>
2021-11-06 20:19:02 -0300

    Add input de file do tipo .jpg

--- jogoteca.py
+++ jogoteca.py
@@ -36,6 +36,8 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     jogo_dao.salvar(jogo)
+    arquivo = request.files['arquivo']
+    arquivo.save(f'uploads/{arquivo.filename}')
     return redirect(url_for('index'))
 
 
