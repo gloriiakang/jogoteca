f23b08a536af47df777cdb74a0f14bd314f26dbf
gloriiakang <lori.vihchoi@gmail.com>
2021-11-06 15:47:25 -0300

    Criando tela de login

--- jogoteca.py
+++ jogoteca.py
@@ -36,4 +36,17 @@ def criar():
     return redirect('/')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+
 app.run(debug=True)
